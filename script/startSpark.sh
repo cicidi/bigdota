a07cd3084cbbb152b3920d9389e4c599a3468f31
cicidi <cicidi@cicidis-iMac.local>
2017-11-15 01:13:53 -0800

    start spark cmd

--- script/startSpark.sh
+++ script/startSpark.sh
@@ -2,3 +2,13 @@ sudo /usr/bin/dse spark
 
 #spark UI
 #http://localhost:4040/jobs/
+
+
+
+
+stant alone spark
+
+master  (dse01)
+/etc/spark-2.2.0-bin-hadoop2.7/sbin/start-master.sh
+slave (mac)
+ /Volumes/WD/app/spark-2.2.0-bin-hadoop2.7sbin/start-slave.sh spark://dse01:7078
