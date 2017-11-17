fe43af6383a29a70d08d3159be866092d79c688b
cicidi <cicidi@cicidis-iMac.local>
2017-11-17 01:14:02 -0800

    comment out useless method

--- src/main/java/com/cicidi/bigdota/cassandra/CassandraConnection.java
+++ src/main/java/com/cicidi/bigdota/cassandra/CassandraConnection.java
@@ -1,20 +1,14 @@
 package com.cicidi.bigdota.cassandra;
 
-import com.cicidi.bigdota.domain.dota.MatchReplay;
 import com.cicidi.bigdota.util.Constants;
 import com.cicidi.bigdota.util.EnvConfig;
-import com.datastax.driver.core.*;
-import com.datastax.spark.connector.japi.CassandraRow;
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.Session;
 import com.sun.jersey.api.client.Client;
-import org.apache.hadoop.util.StringUtils;
 import org.apache.log4j.Logger;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import static com.datastax.spark.connector.japi.CassandraJavaUtil.javaFunctions;
 
 
 /**
@@ -34,22 +28,22 @@ public class CassandraConnection {
     @Autowired
     private Client client;
 
-    public static void main(String[] args) {
-        System.setProperty("hadoop.home.dir", "D:\\project\\hadoop");
-
-        SparkConf conf = new SparkConf().setAppName("bigdota").setMaster("local").set("spark.cassandra.connection.host", "10.0.0.49,10.0.0.38,10.0.0.32");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> cassandraRowsRDD = javaFunctions(sc).cassandraTable(Constants.BIG_DOTA, Constants.REPLAY_TABLE)
-                .map(new Function<CassandraRow, String>() {
-                    @Override
-                    public String call(CassandraRow cassandraRow) throws Exception {
-                        return cassandraRow.toString();
-                    }
-                });
-        System.out.println("done");
-        System.out.println("Data as CassandraRows: \n" + StringUtils.join("\n", cassandraRowsRDD.collect()));
-        System.out.println(cassandraRowsRDD.collect().size());
-    }
+//    public static void main(String[] args) {
+//        System.setProperty("hadoop.home.dir", "D:\\project\\hadoop");
+//
+//        SparkConf conf = new SparkConf().setAppName("bigdota").setMaster("local").set("spark.cassandra.connection.host", "10.0.0.49,10.0.0.38,10.0.0.32");
+//        JavaSparkContext sc = new JavaSparkContext(conf);
+//        JavaRDD<String> cassandraRowsRDD = javaFunctions(sc).cassandraTable(Constants.BIG_DOTA, Constants.REPLAY_TABLE)
+//                .map(new Function<CassandraRow, String>() {
+//                    @Override
+//                    public String call(CassandraRow cassandraRow) throws Exception {
+//                        return cassandraRow.toString();
+//                    }
+//                });
+//        System.out.println("done");
+//        System.out.println("Data as CassandraRows: \n" + StringUtils.join("\n", cassandraRowsRDD.collect()));
+//        System.out.println(cassandraRowsRDD.collect().size());
+//    }
 
 
     private Cluster cluster;
