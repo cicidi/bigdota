a9c3d16868d09191dcaab41647ace87cdea0640f
cicidi <cicidi@cicidis-iMac.local>
2017-11-17 01:13:31 -0800

    comment out useless method

--- src/main/java/com/cicidi/bigdota/TestFile.java
+++ src/main/java/com/cicidi/bigdota/TestFile.java
@@ -1,11 +1,9 @@
 package com.cicidi.bigdota;
 
-import com.cicidi.bigdota.converter.dota.DotaConverter;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 
 public class TestFile {
     public static String read(String path) throws IOException {
@@ -17,8 +15,8 @@ public class TestFile {
 
     public static void main(String[] args) throws IOException {
         String path = "/Volumes/WD/project/bigdota/src/main/resources/matchRawData/2156917527";
-        read(path);
-        Map map = new DotaConverter(read(path)).process();
-        System.out.println(map);
+//        read(path);
+//        Map map = new DotaConverter(read(path)).process();
+        System.out.println(path);
     }
 }
