9d3e16e8470ff0e61a2a81dd5400c2ea40e39b51
cicidi <cicidi@cicidis-iMac.local>
2017-12-30 23:07:51 -0800

    add deploy script

--- dota/script/startSparkCluster.sh
+++ dota/script/startSparkCluster.sh
@@ -1,7 +1,9 @@
-ssh ubuntu03@ubuntu03 "/etc/spark-2.2.0-bin-hadoop2.7/sbin/start-master.sh"
+ssh root@ubuntu03 "/etc/spark-2.2.0-bin-hadoop2.7/sbin/start-master.sh"
 # ignore dse01  for spark because it is too slow adn will decrease whole cluster performance
-#ssh dse01@dse01 "/etc/spark-2.2.0-bin-hadoop2.7/sbin/start-slave.sh spark://ubuntu03:7077"
+ssh dse01@dse01 "/etc/spark-2.2.0-bin-hadoop2.7/sbin/start-slave.sh spark://ubuntu03:7077"
 #ssh dse02@dse02 "/etc/spark-2.2.0-bin-hadoop2.7/sbin/start-slave.sh spark://ubuntu03:7077"
 ssh dse02@dse02 "/home/dse02/spark-2.2.0-bin-hadoop2.7/sbin/start-slave.sh spark://ubuntu03:7077"
 #ssh root@windows "/mnt/d/App/spark-2.2.0-bin-hadoop2.7/sbin/start-slave.sh spark://ubuntu03:7077"
 ssh cicidi@dse03 "/Volumes/WD/app/spark-2.2.0-bin-hadoop2.7/sbin/start-slave.sh spark://ubuntu03:7077"
+
+ssh jianming@10.0.0.30 "/home/jianming/spark-2.2.0-bin-hadoop2.7/sbin/start-slave.sh spark://ubuntu03:7077"
